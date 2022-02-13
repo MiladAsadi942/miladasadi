b91a16c1d371d31ee57d837841d1146f8ed41980
MiladAsadi942 <99612534+MiladAsadi942@users.noreply.github.com>
2022-02-13 10:34:17 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let signs = document.getElementById('conti-hidee');
+let logins = document.getElementById('conti-login');
+let btnsign = document.getElementById('btn-sign');
+let anim = document.getElementById('anim');
+
+
+btnsign.addEventListener('click' , function(e){
+   
+    signs.style.display = 'block';
+    logins.style.display = 'none'; 
+    e.preventDefault();
+   
+})
+
+let btnlogin = document.getElementById('btn-log');
+
+btnlogin.addEventListener('click' , function(e){
+
+    logins.style.display = 'flex';
+    signs.style.display = 'none';
+    e.preventDefault();
+
+
+
+})
+
+let resbtn = document.getElementById('resbtn');
+
+resbtn.addEventListener('click' , function(e){
+    signs.style.display = 'block';
+    logins.style.display = 'none'; 
+    e.preventDefault();
+    
+   
+
+})
+
